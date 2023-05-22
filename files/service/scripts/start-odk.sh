bfa73c5c29c71b7a98e58c2ad6dbfa68e0b42476
Ashley Felton <ashley@ropable.com>
2023-05-22 11:45:23 +0800

    Submodule states

--- files/service/scripts/start-odk.sh
+++ files/service/scripts/start-odk.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 
 echo "generating local service configuration.."
-
 ENKETO_API_KEY=$(cat /etc/secrets/enketo-api-key) \
     envsubst '$DOMAIN $BASE_URL $SYSADMIN_EMAIL $SERVICE_EMAIL $ENKETO_API_KEY $HTTPS_PORT $DB_HOST $DB_USER $DB_PASSWORD $DB_NAME $DB_SSL $EMAIL_HOST $EMAIL_PORT $EMAIL_SECURE' \
     < /usr/share/odk/config.json.template \
     > /usr/odk/config/local.json
 
+export SENTRY_RELEASE="$(cat sentry-versions/server)"
+export SENTRY_TAGS="{ \"version.central\": \"$(cat sentry-versions/central)\", \"version.client\": \"$(cat sentry-versions/client)\" }"
+
 echo "running migrations.."
-node -e 'const { withDatabase, migrate } = require("./lib/model/migrate"); withDatabase(require("config").get("default.database"))(migrate);'
+node ./lib/bin/run-migrations
 
 echo "checking migration success.."
-node -e 'const { withDatabase, checkMigrations } = require("./lib/model/migrate"); withDatabase(require("config").get("default.database"))(checkMigrations);'
+node ./lib/bin/check-migrations
 
 if [ $? -eq 1 ]; then
   echo "*** Error starting ODK! ***"
@@ -32,5 +34,4 @@ fi
 echo "using $WORKER_COUNT worker(s) based on available memory ($MEMTOT).."
 
 echo "starting server."
-pm2-runtime ./pm2.config.js --instances $WORKER_COUNT
-
+pm2-runtime ./pm2.config.js
